c51a7c14dea44c6a287baa13572277b62e778b84
sergioviniciuss <sergioviniciuss@gmail.com>
2017-06-18 18:31:57 -0300

    Bugfix to allow user to change template's choice after already choosing one, without the needing of reloading the page

--- IFC-JIRA-STANDARDS/content.js
+++ IFC-JIRA-STANDARDS/content.js
@@ -7,14 +7,13 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 			tester : "**Status do teste:**\n**Notas do teste:**\n**Evidencias do teste:**"
 		};
 		var chosenTemplate = request.chosenTemplate;
-		//The Code below is specific for IFC JIRA. You need to customize it
+		//The Code below is specific for IFC JIRA Pages. You need to customize it
 		//according to your page details
 		
 		//open comment textarea
 		document.getElementById("comment_issue").click();
-		// document.getElementById("comment").value = "";
 		if (!!templateType[chosenTemplate]) {
-			$("#comment").html(templateType[chosenTemplate]).wrap('<pre />');
+			$("#comment").val(templateType[chosenTemplate]);
 		} else {
 			console.log("Oops.. chosenTemplate mismatch error... Please report it on github");
 		}
